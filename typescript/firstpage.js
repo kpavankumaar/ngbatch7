fc19d86ea9b181db895ca6cfe400693f105b33a8
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-01-02 09:21:50 +0530

    boostrap and typescript

---
+++ typescript/firstpage.js
@@ -0,0 +1,4 @@
+function sum(a, b) {
+    console.log(a + b);
+}
+sum(10, '20');
---
+++ typescript/firstpage.ts
@@ -0,0 +1,9 @@
+// install node from nodejs.org
+// install typescript with npm 
+// tsc to transpile the ts file to js file 
+var num:number;
+num = '';
+function sum(a:number,b:number){
+    console.log(a+b);
+}
+sum(10,'20');
