a7c7e33ddb47c709eea3b0b252f1c23d99ef850d
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-01-03 09:17:25 +0530

    type definitions and interface

--- typescript/firstpage.js
+++ typescript/firstpage.js
@@ -1,4 +1,14 @@
+// install node from nodejs.org
+// install typescript with npm 
+// tsc to transpile the ts file to js file 
+let num = 20;
+num = 30;
 function sum(a, b) {
     console.log(a + b);
 }
 sum(10, '20');
+for(let i = 0; i < 10 ; i++){
+    setTimeout(function(){
+        console.log(i);
+    },1000)
+}
--- typescript/firstpage.ts
+++ typescript/firstpage.ts
@@ -1,9 +1,31 @@
 // install node from nodejs.org
 // install typescript with npm 
 // tsc to transpile the ts file to js file 
-var num:number;
-num = '';
-function sum(a:number,b:number){
+interface ObjDefinition{
+    [key:string]:number|string;
+    
+}
+
+let num:number = 20;
+const name_1:string = 'krishna';
+name_1 = 'ravi';
+const obj1:{name:any} = {
+    name:'krishna',
+    age: 10
+}
+const obj:ObjDefinition = {
+    name: 'ravi',
+    age: 55
+}
+obj.name = 'pavan';
+const friendList:any[] = ['ravi','krishna','radhe',10];
+// const friendList:Array<string|number> = ['ravi','krishna','radhe',10];
+friendList.push('balaram');
+friendList.pop()
+num = 30;
+function sum(a,b:number):number{
     console.log(a+b);
+    return 10;
 }
 sum(10,'20');
+
