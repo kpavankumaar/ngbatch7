2faef6c88dc244912e38e471d219dc790f34b2d4
ngjsmdb <ngjsmdb@gmail.com>
2019-01-18 09:26:37 +0530

    template driven form

--- angular/ngconcepts/src/app/app.module.ts
+++ angular/ngconcepts/src/app/app.module.ts
@@ -1,14 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { TemplatedrivenformComponent } from './templatedrivenform/templatedrivenform.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TemplatedrivenformComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ angular/ngconcepts/src/app/templatedrivenform/templatedrivenform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-templatedrivenform',
+  templateUrl: './templatedrivenform.component.html',
+  styleUrls: ['./templatedrivenform.component.css']
+})
+export class TemplatedrivenformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
