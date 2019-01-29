10b08d5736a67c7a61fecc80edcac8a9e23d3470
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-01-29 09:13:17 +0530

    custom validations

--- angular/ngconcepts/src/app/reactiveform/reactiveform.component.ts
+++ angular/ngconcepts/src/app/reactiveform/reactiveform.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+// function verifyRating(c:AbstractControl):{[key:string]:boolean} | null{
+//   if(typeof(c.value)== 'number'  && c.value < 0 || c.value > 5){
+//    return {range:true}
+//   }
+//   return null;
+// }
 
 @Component({
   selector: 'app-reactiveform',
@@ -9,7 +15,14 @@ import { FormGroup, FormControl } from '@angular/forms';
 export class ReactiveformComponent implements OnInit {
   showAddressDetails:boolean = true;
   customersForm;
-  
+  verifyRating(min,max){
+    return function (c:AbstractControl):{[key:string]:boolean} | null {
+      if(typeof(c.value)== 'number'  && c.value < min || c.value > max){
+      return {range:true}
+      }
+      return null;
+    }
+}
   // showOrHideAddress(){
   //     var verify = this.customersForm.get('verify');
   //     console.log(verify);
@@ -20,9 +33,10 @@ export class ReactiveformComponent implements OnInit {
   ngOnInit() {
     // root form group
     this.customersForm = new FormGroup({
-      firstName : new FormControl('Krishna'),
+      firstName : new FormControl('Krishna',[Validators.required,Validators.minLength(3)]),
       lastName : new FormControl(''),
       emailName : new FormControl(''),
+      rating : new FormControl('',this.verifyRating(0,5)),
       verify:new FormControl(true),
       street : new FormControl(),
       city : new FormControl(),
