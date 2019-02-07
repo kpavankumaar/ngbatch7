639c7d8574f2eaf2ca4f8df57e1d30380375bab4
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-02-07 09:06:51 +0530

    components concepts

---
+++ angular/componentconcepts/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { CustomersComponent } from './customers/customers.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CustomersComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor(){
+    console.log('app module constructor');
+  }
+ }
