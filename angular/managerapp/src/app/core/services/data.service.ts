c6fc717bf5a1befbfeba15e79f3cc14095e3482d
nbitsmeanstack49 <nbitsmeanstack49@gmail.com>
2019-03-25 09:12:12 +0530

    update and delete customers

--- angular/managerapp/src/app/core/services/data.service.ts
+++ angular/managerapp/src/app/core/services/data.service.ts
@@ -12,6 +12,19 @@ export class DataService{
     // logout -> '/api/auth/logout'
     // get customers data url - > '/api/customers'
     // put -> /api/customers/:id
+    // get -> /api/orders/:id
+    // delete -> /api/customers/:id
+    // get states data \
+    // get -> /api/states
+
+    deleteCustomer(id:number){
+        return this.http.delete('/api/customers/'+id);
+    }
+    updateCustomer(customer):Observable<any>{
+        return this.http.put('/api/customers/'+customer.id ,customer).pipe(
+            map((res)=> res)
+        )
+    }
     getCustomersData():Observable<any>{
         return this.http.get('/api/customers');
     }
--- angular/managerapp/src/app/customer/customer-edit.component.ts
+++ angular/managerapp/src/app/customer/customer-edit.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { DataService } from '../core/services/data.service';
 
 @Component({
@@ -8,9 +8,11 @@ import { DataService } from '../core/services/data.service';
   styleUrls: ['./customer-edit.component.css']
 })
 export class CustomerEditComponent implements OnInit {
+  @ViewChild('customerForm') customerForm
   id;
   customer;
-  constructor(private route:ActivatedRoute, private dataService:DataService) { }
+  operationText = "Insert";
+  constructor(private route:ActivatedRoute, private dataService:DataService, private router:Router) { }
 
   ngOnInit() {
     console.log(this.route); 
@@ -22,9 +24,21 @@ export class CustomerEditComponent implements OnInit {
     this.dataService.getCustomer(this.id).subscribe((data) => {
         console.log(data);
         this.customer = data;
+        this.operationText = 'Update';
       } 
     )
     
   }
-
+  submit(){
+    this.dataService.updateCustomer(this.customerForm).subscribe(
+      (res)=>{console.log('inserted customer', res)});
+  }
+  delete(event:Event){
+    this.dataService.deleteCustomer(this.customer.id).subscribe((status:boolean)=>{
+      if(status){
+        this.router.navigate(['/customers'])
+      }
+    }
+    )
+  }
 }
